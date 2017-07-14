3a9cb1aa514ad81ea0f7ae93fe47258cf1f4455e
jwh5566 <jwh5566@foxmail.com>
2017-07-14 17:23:49 +0800

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category
+from .models import Post, Category, Tag
 from comments.forms import CommentForm
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
@@ -73,6 +73,75 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        """
+        类视图的模板变量传递
+        """
+        context = super(IndexView, self).get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')  # 当前页对象
+        is_paginated = context.get('is_paginated')
+
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+
+        left = []
+        right = []
+
+        left_has_more = False
+        right_has_more = False
+
+        first = False
+        last = False
+
+        page_Number = page.number
+        total_pages = paginator.num_pages
+
+        page_range = paginator.page_range
+
+        if page_Number == 1:
+            right = page_range[page_Number:page_Number+2]
+            if right[-1] < total_pages -1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page_Number == total_pages:
+            left = page_range[(page_Number - 3) if (page_Number - 3 > 0) else 0:page_Number - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        else:
+            left = page_range[(page_Number - 3) if (page_Number - 3 > 0) else 0:page_Number - 1]
+            right = page_range[page_Number:page_Number+2]
+
+            if right[-1] < total_pages -1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more':right_has_more,
+            'first': first,
+            'last': last,
+        }
+        return data
 
 
 class CategoryView(ListView):
@@ -99,3 +168,44 @@ class ArchivesView(ListView):
         #                             )
         return super(ArchivesView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'),
         created_time__month=self.kwargs.get('month'))
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request,  *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
+
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wr*j5u09365iv48e)-7&!l*yja-8l*6syl=lg5(!-i(1h&gjl8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 

21001777c287e32fe7e35d0c0e42f4e9cdfc8d0e
jwh5566 <jwh5566@foxmail.com>
2017-07-21 12:30:54 +0800

    sixth commit

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
     url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
+
+
+    url(r'^search/', include('haystack.urls')),
+
 ]
